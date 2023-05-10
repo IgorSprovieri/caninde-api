1315f3b5cd144b0ff785a10fdecee15cd206c71e
Igor Sprovieri <igsp1@hotmail.com>
2023-05-10 09:56:46 -0300

    Chore: Env db config

--- src/db/dataSource.ts
+++ src/db/dataSource.ts
@@ -2,13 +2,15 @@ import { DataSource } from "typeorm";
 import { users } from "./models/users";
 import { rents } from "./models/rents";
 
+const { HOST, PORT, USERNAME, PASSWORD, DATABASE } = process.env;
+
 const appDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "docker",
-  password: "docker",
-  database: "postgres",
+  host: HOST || "localhost",
+  port: Number(PORT) || 5432,
+  username: USERNAME || "docker",
+  password: PASSWORD || "docker",
+  database: DATABASE || "postgres",
   entities: [users, rents],
   synchronize: true,
   logging: false,
